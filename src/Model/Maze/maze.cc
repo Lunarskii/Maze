2a930cb051365d29696b1f26324931ccfaa61eb1
Lunarskii <lunarskii0@gmail.com>
2023-08-23 16:53:41 +0300

    Now generation and opening via file are in a separate class

--- src/Model/Maze/maze.cc
+++ src/Model/Maze/maze.cc
@@ -8,55 +8,25 @@ typename Maze::Maze& Maze::GetInstance()
 
 void Maze::UploadMazeFromFile(std::string file_name)
 {
-    FileReader(file_name).Upload(&right_walls_, &bottom_walls_);
+    maze_ = MazeFileManager::Upload(file_name);
+
+    if (!maze_.IsValid())
+    {
+        throw std::exception();
+    }
 }
 
 void Maze::GenerateMaze(unsigned int rows, unsigned int cols)
 {
-    MazeGenerator generator(rows, cols);
-    generator.GenerateMaze();
-    right_walls_ = generator.GetRightWalls();
-    bottom_walls_ = generator.GetBottomWalls();
-    rows_ = generator.GetRows();
-    cols_ = generator.GetCols();
+    maze_ = MazeGenerator().Generate(rows, cols);
 }
 
-void Maze::SaveToFile(std::string file_name)
+void Maze::SaveMaze(std::string file_name)
 {
-    std::ofstream file(file_name);
-
-    if (!file.is_open()) 
-    {
-        throw std::exception(); // обработать исключение
-        // std::cout << "file not created" << std::endl;
-        // return;
-    }
-
-    file << rows_ << " " << cols_;
-    WriteDataToFile_(file, right_walls_);
-    file << "\n";
-    WriteDataToFile_(file, bottom_walls_);
-    file.close();
+    MazeFileManager::Save(file_name, maze_);
 }
 
-void Maze::WriteDataToFile_(std::ofstream& file, maze_type& vec)
+MazeType& Maze::GetMaze()
 {
-    for (const auto& vectors : vec)
-    {
-        file << "\n";
-        for (const auto& data : vectors)
-        {
-            file << data << " ";
-        }
-    }
-}
-
-std::vector<std::vector<bool>>& Maze::GetRightWalls()
-{
-    return right_walls_;
-}
-
-std::vector<std::vector<bool>>& Maze::GetBottomWalls()
-{
-    return bottom_walls_;
+    return maze_;
 }
--- src/Model/Maze/maze.h
+++ src/Model/Maze/maze.h
@@ -1,37 +1,20 @@
 #ifndef A1_MAZE_MODEL_MAZE_MAZE_H_
 #define A1_MAZE_MODEL_MAZE_MAZE_H_
 
-#define MIN_WIDGET_SIZE 1
-#define MAX_WIDGET_SIZE 50
-
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <random> // BoolRandom()
-#include "../FileReader/filereader.h"
-#include "maze_generator.h"
-
-#include <iostream>
+#include "../file_manager.h"
+#include "../generator.h"
 
 class Maze
 {
 public:
-    using maze_type = std::vector<std::vector<bool>>;
-
     static Maze& GetInstance();
     void UploadMazeFromFile(std::string file_name);
     void GenerateMaze(unsigned int rows, unsigned int cols);
-    void SaveToFile(std::string file_name);
-    maze_type& GetRightWalls();
-    maze_type& GetBottomWalls();
+    void SaveMaze(std::string file_name);
+    MazeType& GetMaze();
 
 private:
-    maze_type right_walls_;
-    maze_type bottom_walls_;
-    unsigned int rows_;
-    unsigned int cols_;
-
-    void WriteDataToFile_(std::ofstream& file, maze_type& vec);
+    MazeType maze_;
 };
 
 
