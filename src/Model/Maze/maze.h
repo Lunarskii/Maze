00311ce9e35f239b93dd9ec107c60cdaf394f4d6
softsunami <maxon1107@yandex.ru>
2023-09-19 21:08:00 +0300

    add doxy for mazes

--- src/Model/Maze/maze.h
+++ src/Model/Maze/maze.h
@@ -5,18 +5,55 @@
 #include "../generator.h"
 #include "path_finder.h"
 
-class Maze
-{
-public:
-    static Maze& GetInstance();
-    void UploadMazeFromFile(std::string file_name);
-    void GenerateMaze(unsigned int rows, unsigned int cols);
-    void SaveMaze(std::string file_name);
-    std::vector<Point> FindPath(Point from, Point to);
-    MazeType& GetMaze();
+/**
+ * @class Maze
+ * @brief Сlass for processing mazes
+ */
+class Maze {
+ public:
+  /**
+   * @brief Getting an instance of an object (can only be in a single quantity).
+   * @return Reference to the object.
+   */
+  static Maze& GetInstance();
 
-private:
-    MazeType maze_;
+  /**
+   * @brief Loads a maze from a text file.
+   * @param file_name The path to the file.
+   */
+  void UploadMazeFromFile(std::string file_name);
+
+  /**
+   * @brief Generates a maze based on the given arguments (rows, cols).
+   * @param rows Сan have values from 1 to 50.
+   * @param cols Сan have values from 1 to 50.
+   */
+  void GenerateMaze(unsigned int rows, unsigned int cols);
+
+  /**
+   * @brief Saves the maze to a file.
+   * @param file_name The name of the file where the maze will be saved.
+   */
+  void SaveMaze(std::string file_name);
+
+  /**
+   * @brief Returns the vector of points that make up the route from the first
+   * point to the second.
+   * @param from The point where the path starts from.
+   * @param to The point where the path ends.
+   * @return Vector of points that make up the route from the first point to the
+   * second.
+   */
+  std::vector<Point> FindPath(Point from, Point to);
+
+  /**
+   * @brief Returns a reference to the MazeType.
+   * @return Reference to the MazeType.
+   */
+  MazeType& GetMaze();
+
+ private:
+  MazeType maze_;
 };
 
-#endif //  A1_MAZE_MODEL_MAZE_MAZE_H_
+#endif  //  A1_MAZE_MODEL_MAZE_MAZE_H_
